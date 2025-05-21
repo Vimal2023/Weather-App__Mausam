44bb83fa9b322b03a5039dbcfd2063c99c6ec1cc
Vimal2023 <vimal83500@gmail.com>
2025-05-21 11:44:54 +0530

    layout fix

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,14 +3,23 @@ import { Header } from "./header";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="bg-gradient-to-br from-background to-muted">
-      <Header/>
-      <main className="min-h-screen container mx-auto px-4 py-8">
+    <div className="bg-gradient-to-br from-background to-muted min-h-screen">
+      <Header />
+      <main className="min-h-screen container mx-auto px-2 sm:px-4 py-8">
         {children}
       </main>
       <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
-        <div className="container mx-auto px-4 text-center text-gray-400">
-          <p> Made by ❤️Code by <a href="https://portfolio-c-vimal-anand.vercel.app/" target="_blank" className="text-decoration: underline text-amber-800">Vimal Anand</a></p>
+        <div className="container mx-auto px-2 sm:px-4 text-center text-gray-400">
+          <p>
+            Made by ❤️ Code by{" "}
+            <a
+              href="https://portfolio-c-vimal-anand.vercel.app/"
+              target="_blank"
+              className="underline text-amber-800"
+            >
+              Vimal Anand
+            </a>
+          </p>
         </div>
       </footer>
     </div>
