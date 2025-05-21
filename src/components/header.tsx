e027656e32441f9b772dd5d45738ee24d7547a27
Vimal2023 <vimal83500@gmail.com>
2025-05-21 11:39:23 +0530

    navbar fixing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,16 +8,16 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
-      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+      <div className="container mx-auto flex h-12 sm:h-16 items-center justify-between px-2 sm:px-4">
         <Link to={"/"}>
           <img
             src={theme === "dark" ? "/logom.png" : "/logom.png"}
             alt="Mausam logo"
-            className="h-14"
+            className="h-10 sm:h-14"
           />
         </Link>
 
-        <div className="flex gap-4">
+        <div className="flex gap-2 sm:gap-4">
           <CitySearch />
           <ThemeToggle />
         </div>
